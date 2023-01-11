1c8573779d7bdc8e5302b7dca66a0b4f4580aaff
Pabin R Luitel <pabin2050@gmail.com>
2023-01-11 22:24:18 +0545

    comment remote domain

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,55 +1,65 @@
-const HTTP_OR_HTTPS = "https";
-const DOMAIN_NAME = "yummipizza-be-server.herokuapp.com";
-// const DOMAIN_NAME = "localhost:8000";
-
+const HTTP_OR_HTTPS = "http";
+// const DOMAIN_NAME = "yummipizza-be-server.herokuapp.com";
+const DOMAIN_NAME = "localhost:8000";
 
 // Accounts
-const getAuthenticationUrl = () => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/accounts/api/authentication/`;
-const getUserRetrieveOrUpdateUrl = (user_id) => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/accounts/api/user/${user_id}/`;
-const getUserSignupUrl = () => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/accounts/api/signup/`;
+const getAuthenticationUrl = () =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/accounts/api/authentication/`;
+const getUserRetrieveOrUpdateUrl = (user_id) =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/accounts/api/user/${user_id}/`;
+const getUserSignupUrl = () =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/accounts/api/signup/`;
 
 // Inventory
-const getItemListUrl = () => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/items/`;
-const getItemFilterUrl = () => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/items-filter/`;
-const getItemViewsUpdateUrl = (item_id) => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/item/${item_id}/`;
-const getPopularItemListUrl = () => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/popular-items/`;
+const getItemListUrl = () =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/items/`;
+const getItemFilterUrl = () =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/items-filter/`;
+const getItemViewsUpdateUrl = (item_id) =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/item/${item_id}/`;
+const getPopularItemListUrl = () =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/popular-items/`;
 
-const getShoppingCartCreateUrl = () => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/cart/`;
-const getCartRetrieveOrUpdateUrl = (cart_id) => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/cart/${cart_id}/`;
-const getCartItemRetrieveOrUpdateUrl = (cart_item_id) => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/cart-item/${cart_item_id}/`;
+const getShoppingCartCreateUrl = () =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/cart/`;
+const getCartRetrieveOrUpdateUrl = (cart_id) =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/cart/${cart_id}/`;
+const getCartItemRetrieveOrUpdateUrl = (cart_item_id) =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/cart-item/${cart_item_id}/`;
 
-const getOrderCreateUrl = () => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/order/`;
-const getUserOrderListUrl = () => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/user-orders/`;
-const getUserOrderFilterUrl = () => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/order-filter/`;
+const getOrderCreateUrl = () =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/order/`;
+const getUserOrderListUrl = () =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/user-orders/`;
+const getUserOrderFilterUrl = () =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/inventory/api/order-filter/`;
 
 // Reviews and Ratings
-const getItemReviewListUrl = () => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/reviews/api/items-reviews/`;
-const getItemReviewCreateUrl = (item_id) => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/reviews/api/review/${item_id}/`;
-const getItemRatingListUrl = () => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/reviews/api/items-ratings/`;
-const getItemRatingCreateUrl = (item_id) => `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/reviews/api/rating/${item_id}/`;
-
-
+const getItemReviewListUrl = () =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/reviews/api/items-reviews/`;
+const getItemReviewCreateUrl = (item_id) =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/reviews/api/review/${item_id}/`;
+const getItemRatingListUrl = () =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/reviews/api/items-ratings/`;
+const getItemRatingCreateUrl = (item_id) =>
+  `${HTTP_OR_HTTPS}://${DOMAIN_NAME}/reviews/api/rating/${item_id}/`;
 
 export {
   getAuthenticationUrl,
   getUserRetrieveOrUpdateUrl,
   getUserSignupUrl,
-
   getItemListUrl,
   getItemFilterUrl,
   getItemViewsUpdateUrl,
   getPopularItemListUrl,
-
   getShoppingCartCreateUrl,
   getCartRetrieveOrUpdateUrl,
   getCartItemRetrieveOrUpdateUrl,
-
   getOrderCreateUrl,
   getUserOrderListUrl,
   getUserOrderFilterUrl,
-
   getItemReviewListUrl,
   getItemReviewCreateUrl,
   getItemRatingListUrl,
   getItemRatingCreateUrl,
-}
+};
